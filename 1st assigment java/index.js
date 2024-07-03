8e6977cdb3c5d70d70333e0e809248ef80d6c3eb
hoorain shahid <hoorainshahid470@gmail.com>
2024-07-03 06:57:11 -1200

    javascript assigment

---
+++ 1st assigment java/index.js
@@ -0,0 +1,30 @@
+
+var password = "Enter your valid password"  
+console
+
+
+
+
+alert("Enter your valid password")
+alert("welcome to js land.... happy coding")
+alert("welcome to js land.... ")
+alert(" happy coding prevent this page from creating additional dialogs.")
+alert("hello... i can run js through my web browsers console")
+
+
+
+// CHAP TWO VARIABLES FOR  STRING
+
+alert("jhone doe")
+alert("15 years old")
+alert("cirtified mobile application development")
+
+// USE SE INPUT  LENE K LIYE
+
+
+var c = +prompt()
+console.log(c+3)
+
+
+var c = prompt('enter your  name')
+console.log(c)
---
+++ weather tampreture chart/index.js
@@ -0,0 +1,31 @@
+
+
+var wethear = "hot"
+console.log(wethear)
+document.write(wethear)
+
+
+
+
+
+
+document.write('<div class="card">' +
+
+
+
+'</div>'
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
